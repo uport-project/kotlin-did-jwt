179295064a78952b719e5346400e59bf9ca29c2d
Mircea Nistor <mirceanis@gmail.com>
2019-07-22 17:17:37 +0200

    add ArbitraryMapSerializer to cover generic JSON trees

---
+++ jwt/src/main/java/me/uport/sdk/jwt/model/ArbitraryMapSerializer.kt
@@ -0,0 +1,81 @@
+package me.uport.sdk.jwt.model
+
+import kotlinx.serialization.*
+import kotlinx.serialization.internal.LinkedHashMapClassDesc
+import kotlinx.serialization.internal.StringDescriptor
+import kotlinx.serialization.json.*
+import kotlinx.serialization.modules.getContextualOrDefault
+
+/**
+ * This handles serialization and deserialization of arbitrary JSON trees represented as
+ * `Map<String, Any?>`
+ *
+ * Serialization involves [ImplicitReflectionSerializer] so this will probably
+ * not work in a multi-platform environment.
+ *
+ * Using `@Serializable` objects in the tree respects their declared serializers.
+ *
+ * **De-serialization is not reliable for primitive types.**
+ * Primitives that can be represented as `Boolean` or `Long` will be coerced into those types,
+ * even if they are actually Strings in the original representation.
+ *
+ */
+object ArbitraryMapSerializer : KSerializer<Map<String, Any?>> {
+
+    ////////////////////////
+    // converting to Json //
+    ////////////////////////
+    @ImplicitReflectionSerializer
+    private fun Map<*, *>.toJsonObject(): JsonObject = JsonObject(map {
+        it.key.toString() to it.value.toJsonElement()
+    }.toMap())
+
+    @ImplicitReflectionSerializer
+    private fun Any?.toJsonElement(): JsonElement = when (this) {
+        null -> JsonNull
+        is Number -> JsonPrimitive(this)
+        is String -> JsonPrimitive(this)
+        is Boolean -> JsonPrimitive(this)
+        is Map<*, *> -> this.toJsonObject()
+        is Iterable<*> -> JsonArray(this.map { it.toJsonElement() })
+        is Array<*> -> JsonArray(this.map { it.toJsonElement() })
+        else -> {
+            //supporting classes that declare serializers
+            val jsonParser = Json(JsonConfiguration.Stable)
+            val serializer = jsonParser.context.getContextualOrDefault(this)
+            jsonParser.toJson(serializer, this)
+        }
+    }
+
+    //////////////////////////////////////////////////
+    // converting back to primitives (no type info) //
+    //////////////////////////////////////////////////
+    private fun JsonObject.toPrimitiveMap(): Map<String, Any?> =
+        this.content.map {
+            it.key to it.value.toPrimitive()
+        }.toMap()
+
+    private fun JsonElement.toPrimitive(): Any? = when (this) {
+        is JsonNull -> null
+        is JsonObject -> this.toPrimitiveMap()
+        is JsonArray -> this.map { it.toPrimitive() }
+        //FIXME: how do I get this to use the proper primitive type?
+        is JsonPrimitive -> booleanOrNull ?: longOrNull ?: doubleOrNull ?: contentOrNull
+        else -> null
+    }
+
+    override fun deserialize(decoder: Decoder): Map<String, Any?> {
+        val asJsonObject: JsonObject = decoder.decodeSerializableValue(JsonObjectSerializer)
+        return asJsonObject.toPrimitiveMap()
+    }
+
+    override val descriptor: SerialDescriptor
+        get() = LinkedHashMapClassDesc(StringDescriptor, PolymorphicClassDescriptor)
+
+    @ImplicitReflectionSerializer
+    override fun serialize(encoder: Encoder, obj: Map<String, Any?>) {
+        val asJsonObj: JsonObject = obj.toJsonObject()
+        encoder.encode(JsonObjectSerializer, asJsonObj)
+    }
+
+}
---
+++ jwt/src/test/java/me/uport/sdk/jwt/SerializationTesting.kt
@@ -0,0 +1,74 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "EXPERIMENTAL_API_USAGE")
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import me.uport.sdk.jwt.model.ArbitraryMapSerializer
+import org.junit.Test
+
+class SerializationTesting {
+
+    @Serializable
+    data class ClassWithSerializer(
+
+        //test serializtion of annotated types
+        @SerialName("@context")
+        val context: List<String>
+    )
+
+    @Serializable
+    data class CompoundTestObject(
+
+        //use custom serializers for arbitrary map types
+        @Serializable(with = ArbitraryMapSerializer::class)
+        val generic: Map<String, Any?>
+    )
+
+
+
+
+    @Test
+    fun `can serialize object with any map`() {
+        val aa = CompoundTestObject(
+            mapOf(
+                "hello" to "world",
+                "missing" to null,
+                "some number" to 4321,
+                "number as string" to "1234",
+                "boolean" to false,
+                "boolean as string" to "true",
+                "custom object" to ClassWithSerializer(listOf("asdf")),
+                "obj" to mapOf(
+                    "a" to "b",
+                    "c" to null
+                )
+            )
+        )
+        val serialized = Json.stringify(CompoundTestObject.serializer(), aa)
+        assertThat(serialized).isEqualTo("""{"generic":{"hello":"world","missing":null,"some number":4321,"number as string":"1234","boolean":false,"boolean as string":"true","custom object":{"@context":["asdf"]},"obj":{"a":"b","c":null}}}""")
+    }
+
+    @Test
+    fun `can deserialize known object`() {
+        val input = """{"generic":{"hello":"world","missing":null,"some number":4321,"number as string":"1234","boolean":false,"boolean as string":"true","custom object":{"@context":["asdf"]},"obj":{"a":"b","c":null}}}"""
+        val parsed = Json.nonstrict.parse(CompoundTestObject.serializer(), input)
+        assertThat(parsed.generic).isEqualTo(
+            mapOf(
+                "hello" to "world",
+                "missing" to null,
+                "some number" to 4321L,
+                "number as string" to 1234L,
+                "boolean" to false,
+                "boolean as string" to true,
+                "custom object" to mapOf("@context" to listOf("asdf")),
+                "obj" to mapOf(
+                    "a" to "b",
+                    "c" to null
+                )
+            )
+        )
+    }
+
+}
