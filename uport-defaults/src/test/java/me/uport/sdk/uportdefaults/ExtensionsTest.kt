e60035cee83593523d0dbc89c0c80939b3312b32
Ugo Amanoh <ugo@graystatic.co>
2019-11-20 13:46:20 +0100

    Fixed failing reference in tests

--- uport-defaults/src/test/java/me/uport/sdk/uportdefaults/ExtensionsTest.kt
+++ uport-defaults/src/test/java/me/uport/sdk/uportdefaults/ExtensionsTest.kt
@@ -1,6 +1,7 @@
 package me.uport.sdk.uportdefaults
 
 import assertk.assertThat
+import assertk.assertions.isTrue
 import me.uport.sdk.universaldid.DIDResolver
 import org.junit.Test
 
@@ -10,12 +11,28 @@ class ExtensionsTest {
     fun `can resolve valid dids configuring with infuraId`() {
 
         val infuraProjectId = "e72b472993ff46d3b5b88faa47214d7f"
-        val resolver = DIDResolver.configureDefaultsWithInfura(infuraProjectId)
+
+        val resolver = DIDResolver.Builder().build().configureDefaultsWithInfura(infuraProjectId)
 
         val dids = listOf(
             "did:ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a",
             "did:ethr:0xB9C5714089478a327F09197987f16f9E5d936E8a",
             "did:ethr:0xB9C5714089478a327F09197987f16f9E5d936E8a#owner",
+            "did:ethr:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+            "did:ethr:0xB9C5714089478a327F09197987f16f9E5d936E8a",
+            "did:ethr:0xB9C5714089478a327F09197987f16f9E5d936E8a#owner",
+            "did:ethr:mainnet:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+            "did:ethr:mainnet:0xB9C5714089478a327F09197987f16f9E5d936E8a",
+            "did:ethr:mainnet:0xB9C5714089478a327F09197987f16f9E5d936E8a#owner",
+            "did:ethr:rinkeby:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+            "did:ethr:rinkeby:0xB9C5714089478a327F09197987f16f9E5d936E8a",
+            "did:ethr:rinkeby:0xB9C5714089478a327F09197987f16f9E5d936E8a#owner",
+            "did:ethr:0x1:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+            "did:ethr:0x1:0xB9C5714089478a327F09197987f16f9E5d936E8a",
+            "did:ethr:0x1:0xB9C5714089478a327F09197987f16f9E5d936E8a#owner",
+            "did:ethr:0x04:0xb9c5714089478a327f09197987f16f9e5d936e8a",
+            "did:ethr:0x04:0xB9C5714089478a327F09197987f16f9E5d936E8a",
+            "did:ethr:0x04:0xB9C5714089478a327F09197987f16f9E5d936E8a#owner",
             "did:https:example.com",
             "did:https:example.ngrok.com#owner",
             "did:web:example.com",
