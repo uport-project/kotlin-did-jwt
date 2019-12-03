308b7e05be34172fc0ac771fc0d5cd0606dcf14a
Ugo Amanoh <ugo@graystatic.co>
2019-12-03 15:29:46 +0100

    Added dummy test to enable jacoco build successfully

---
+++ credential-status/src/test/java/me/uport/sdk/credentialstatus/Tests.kt
@@ -0,0 +1,12 @@
+package me.uport.sdk.credentialstatus
+
+import org.junit.Test
+
+class ExtensionsTest {
+
+    @Test
+    fun `dummy test for jacoco buil report`() {
+        val test = true
+        assert(test)
+    }
+}
