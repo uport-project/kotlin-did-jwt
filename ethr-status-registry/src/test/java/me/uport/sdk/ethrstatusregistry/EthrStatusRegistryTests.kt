eceefbf6938559c18add5aa3f3a817643fdd7fcc
Ugo Amanoh <ugochukwu.amanoh@consensys.net>
2019-12-17 22:48:13 +0100

    test: fixed breaking test

--- ethr-status-registry/src/test/java/me/uport/sdk/ethrstatusregistry/EthrStatusRegistryTests.kt
+++ ethr-status-registry/src/test/java/me/uport/sdk/ethrstatusregistry/EthrStatusRegistryTests.kt
@@ -1,16 +1,19 @@
 package me.uport.sdk.ethrstatusregistry
 
-import kotlinx.coroutines.runBlocking
+import assertk.assertThat
+import assertk.assertions.isInstanceOf
+import me.uport.credential_status.CredentialStatus
 import org.junit.Test
 
 class EthrStatusRegistryTests {
 
     @Test
-    fun `can check for revocation`() = runBlocking {
+    fun `can check for revocation`() {
 
         val validShareReqToken =
             "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJjbGFpbXMiOnsibmFtZSI6IlIgRGFuZWVsIE9saXZhdyJ9LCJzdGF0dXMiOnsidHlwZSI6IkV0aHJTdGF0dXNSZWdpc3RyeTIwMTkiLCJpZCI6ImRpZDpldGhyOjB4ZThjOTFiZGU3NjI1YWIyYzBlZDlmMjE0ZGViMzk0NDBkYTdlMDNjNCJ9LCJpYXQiOjEyMzQ1Njc4LCJleHAiOjEyMzQ1OTc4LCJpc3MiOiJkaWQ6ZXRocjoweDQxMjNjYmQxNDNiNTVjMDZlNDUxZmYyNTNhZjA5Mjg2YjY4N2E5NTAifQ.iRuhl5JHPIBLP_WSr3vRwM8MWTKwJUAUk5AzDDuRvyySsQXuZ-W3Ri3H9WISytZjwHy3ZAnBTcZ6_gkTfdvYhAE"
         val statusRegistry = EthrStatusRegistry()
         val result = statusRegistry.checkStatus(validShareReqToken)
+        assertThat(result).isInstanceOf(CredentialStatus::class.java)
     }
 }
